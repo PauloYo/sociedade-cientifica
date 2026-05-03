39b04ef83805b9197be04e8eee45132788e5555a
Pedro Ferreira <ferfonhp@gmail.com>
2026-05-03 11:25:12 -0300

    feat: fazendo a conexão ao mongodb

--- backend/connect.py
+++ backend/connect.py
@@ -1,14 +1,11 @@
-# from pymongo import MongoClient
-
-# # Replace with your connection string
-# client = MongoClient("mongodb://localhost:27017/") 
-# db = client["my_database"]
-# collection = db["my_collection"]
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://pedro-user:123@bancosangue.eryecey.mongodb.net/?appName=BancoSangue"
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+uri = getenv("MONGODB_URI")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
