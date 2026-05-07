97a45c45844bbaa898f691fdc6ccb9759a897339
Paulo Sousa Sanches Lopes <20231BSI0031@cefetes.br>
2026-05-07 09:12:00 -0300

    feat: mapeamento parcial das rotas e ajustes no modelo

--- src/backend/api/routes.py
+++ src/backend/api/routes.py
@@ -1,14 +1,130 @@
 from fastapi import APIRouter
-from src.backend.api.controller.DoadorController import DoadorController
+
 
 doadorController = DoadorController()
 
 router = APIRouter()
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Post
 
-@router.get("/doador")
-def getDoador():
-    return {'response': doadorController.findOne()}
+@router.post("/area")
+def criar_area(item):
+    # TODO
+    return None
+
+@router.post("/pesquisa")
+def criar_pesquisa(item):
+    # TODO
+    return None
+
+@router.post("/teses")
+def criar_teses(item):
+    # TODO
+    return None
+
+@router.post("/livros")
+def criar_livros(item):
+    # TODO
+    return None
+
+@router.post("/artigos")
+def criar_artigos(item):
+    # TODO
+    return None
+
+@router.post("/softwares-tutoriais")
+def criar_softwares_tutoriais(item):
+    # TODO
+    return None
+
+# Listagem
+
+@router.get("/area")
+def criar_area():
+    # TODO
+    return None
+
+@router.get("/pesquisas")
+def listar_pesquisas():
+    # TODO
+    return None 
+
+@router.get("/teses")
+def listar_teses():
+    # TODO
+    return None
+
+@router.get("/livros")
+def listar_livros():
+    # TODO
+    return None
+
+@router.get("/artigos")
+def listar_artigos():
+    # TODO
+    return None
+
+@router.get("/softwares-tutoriais")
+def listar_softwares_tutoriais():
+    # TODO
+    return None
+
+# Listagem - Filtro
+
+@router.get("/pesquisas/{area}")
+def listar_pesquisas_area(area):
+    # TODO
+    return None
+
+@router.get("/teses/{area}")
+def listar_teses_area(area):
+    # TODO
+    return None
+
+@router.get("/livros/{area}")
+def listar_livros_area(area):
+    # TODO
+    return None
+
+@router.get("/artigos/{area}")
+def listar_artigos_area(area):
+    # TODO
+    return None
+
+@router.get("/softwares-tutoriais/{area}")
+def listar_softwares_tutoriais_area(area):
+    # TODO
+    return None
+    
+# Consultas - ID
+
+@router.get("/area/{id}")
+def criar_area(id):
+    # TODO
+    return None
+
+@router.get("/pesquisas/{id}")
+def buscar_pesquisa(id):
+    # TODO
+    return None
+
+@router.get("/teses/{id}")
+def buscar_tese(id):
+    # TODO
+    return None
+
+@router.get("/livros/{id}")
+def buscar_livro(id):
+    # TODO
+    return None
+
+@router.get("/artigos/{id}")
+def buscar_artigo(id):
+    # TODO
+    return None
+
+@router.get("/softwares-tutoriais/{id}")
+def buscar_softwares_tutoriais(id):
+    # TODO
+    return None
+
